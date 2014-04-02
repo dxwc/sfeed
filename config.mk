e7a756c2664abf6baa0da6824030b990e2825501
Hiltjo Posthuma <hiltjo@codemadness.org>
2014-04-02 22:57:51 +0200

    config.mk: make compatible with mkbuild
    Signed-off-by: Hiltjo Posthuma <hiltjo@codemadness.org>

--- config.mk
+++ config.mk
@@ -17,8 +17,8 @@ LIBS = -lc
 #LDFLAGS = ${LIBS}
 
 # optimized
-CFLAGS += -O2 -std=c99 -DVERSION=\"${VERSION}\" -D_POSIX_C_SOURCE=200809L
-LDFLAGS += -s ${LIBS}
+CFLAGS = -O2 -std=c99 -DVERSION=\"${VERSION}\" -D_POSIX_C_SOURCE=200809L
+LDFLAGS = -s ${LIBS}
 
 # Solaris
 #CFLAGS = -fast ${INCS} -DVERSION=\"${VERSION}\"
