a7aad223c584be7b5acdf7fc41397853425d45e8
Hiltjo Posthuma <hiltjo@codemadness.org>
2015-01-03 12:44:57 +0100

    code style

--- util.h
+++ util.h
@@ -9,23 +9,23 @@
 
 /* feed info */
 struct feed {
-	char *name; /* feed name */
-	unsigned long totalnew; /* amount of new items per feed */
-	unsigned long total; /* total items */
-	time_t timenewest;
-	char timenewestformat[64];
-	struct feed *next; /* linked list */
+	char *         name;     /* feed name */
+	unsigned long  totalnew; /* amount of new items per feed */
+	unsigned long  total;    /* total items */
+	time_t         timenewest;
+	char           timenewestformat[64];
+	struct feed *  next;     /* linked list */
 };
 
 enum { FieldUnixTimestamp = 0, FieldTimeFormatted, FieldTitle, FieldLink,
        FieldContent, FieldContentType, FieldId, FieldAuthor, FieldFeedType,
        FieldFeedName, FieldFeedUrl, FieldBaseSiteUrl, FieldLast };
 
-void feedsfree(struct feed *);
 unsigned int parseline(char **, size_t *, char **, unsigned int, int, FILE *);
-void printfeednameid(const char *, FILE *);
-void printhtmlencoded(const char *, FILE *);
-void printlink(const char *, const char *, FILE *);
-void printutf8pad(FILE *, const char *, size_t, int);
-const char *trimstart(const char *);
-const char *trimend(const char *);
+void         printfeednameid(const char *, FILE *);
+void         printhtmlencoded(const char *, FILE *);
+void         printlink(const char *, const char *, FILE *);
+void         printutf8pad(FILE *, const char *, size_t, int);
+int          strtotime(const char *, time_t *);
+const char * trimstart(const char *);
+const char * trimend(const char *);
