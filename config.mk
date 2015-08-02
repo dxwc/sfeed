22509ae8ab76f452f34947e95f618adf9605fa67
Hiltjo Posthuma <hiltjo@codemadness.org>
2015-08-02 13:46:34 +0200

    config.mk: make optimized the default

--- config.mk
+++ config.mk
@@ -8,14 +8,14 @@ MANPREFIX = ${PREFIX}/share/man
 LIBS = -lc
 
 # debug
-CFLAGS = -fstack-protector-all -O0 -g -std=c99 -Wall -Wextra -pedantic \
-	-DVERSION=\"${VERSION}\" -D_POSIX_C_SOURCE=200809L -D_XOPEN_SOURCE=700 -D_BSD_SOURCE
-LDFLAGS = ${LIBS}
+#CFLAGS = -fstack-protector-all -O0 -g -std=c99 -Wall -Wextra -pedantic \
+#	-DVERSION=\"${VERSION}\" -D_POSIX_C_SOURCE=200809L -D_XOPEN_SOURCE=700 -D_BSD_SOURCE
+#LDFLAGS = ${LIBS}
 
 # optimized
-#CFLAGS = -O2 -std=c99 \
-#	-DVERSION=\"${VERSION}\" -D_POSIX_C_SOURCE=200809L -D_XOPEN_SOURCE=700 -D_BSD_SOURCE
-#LDFLAGS = -s ${LIBS}
+CFLAGS = -O2 -std=c99 \
+	-DVERSION=\"${VERSION}\" -D_POSIX_C_SOURCE=200809L -D_XOPEN_SOURCE=700 -D_BSD_SOURCE
+LDFLAGS = -s ${LIBS}
 
 # optimized static
 #CFLAGS = -static -O2 -std=c99 \
