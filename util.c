dxwc/sfeed

commit 7f11ef506465896705f15c39bd0416d96ca651a8

util: just zero strings by null-terminating first byte

util.c
@@ -28,7 +28,7 @@ parseuri(const char *s, struct uri *u, int rel)
 	const char *p = s;
 	size_t i;
 
-	memset(u, 0, sizeof(struct uri));
+	u->proto[0] = u->host[0] = u->path[0] = '\0';
 	if (!*s)
 		return 0;
 

commit 5c724b8b1aba860eb8a48dc230fa417014a11ba7

util: parseuri: nul-terminate, bug introduced by 7f11ef506465896705f15c39bd0416d96ca651a8

util.c
@@ -44,6 +44,7 @@ parseuri(const char *s, struct uri *u, int rel)
 			if (p - s + 1 >= (ssize_t)sizeof(u->proto))
 				return -1; /* protocol too long */
 			memcpy(u->proto, s, p - s);
+			u->proto[p - s] = '\0';
 			p += 3; /* skip "://" */
 		} else {
 			p = s; /* no protocol format, set to start */
@@ -57,6 +58,7 @@ parseuri(const char *s, struct uri *u, int rel)
 	if (i + 1 >= sizeof(u->host))
 		return -1; /* host too long */
 	memcpy(u->host, p, i);
+	u->host[i] = '\0';
 	p = &p[i];
 
 readpath: