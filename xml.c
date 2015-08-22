fddbcbb7125413780de042c410adb75ed0ea055f
Hiltjo Posthuma <hiltjo@codemadness.org>
2015-08-22 20:13:18 +0200

    xml: fix includes

--- xml.c
+++ xml.c
@@ -1,10 +1,10 @@
 #include <ctype.h>
 #include <errno.h>
+#include <limits.h>
 #include <stdint.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include <unistd.h>
 
 #include "xml.h"
 
