9a74fe65fee19f5b8d71c6c379ca028ad0e356a4
Hiltjo Posthuma <hiltjo@codemadness.org>
2016-03-10 18:52:23 +0100

    Makefile: define CPPFLAGS once, fix typo

--- config.mk
+++ config.mk
@@ -10,19 +10,18 @@ AR = ar
 RANLIB = ranlib
 
 # debug
-#CFLAGS = -fstack-protector-all -O0 -g -std=c99 -Wall -Wextra -pedantic \
-#	-D_POSIX_C_SOURCE=200809L -D_XOPEN_SOURCE=700 -D_BSD_SOURCE
+#CFLAGS = -fstack-protector-all -O0 -g -std=c99 -Wall -Wextra -pedantic
 #LDFLAGS =
 
 # optimized
 CFLAGS = -O2 -std=c99
-CPPFLAGS = -D_POSIX_C_SOURCE=200809L -D_XOPEN_SOURCE=700 -D_BSD_SOURCE
 LDFLAGS = -s
 
 # optimized static
 #CFLAGS = -static -O2 -std=c99
-CPPFLAGS = -D_POSIX_C_SOURCE=200809L -D_XOPEN_SOURCE=700 -D_BSD_SOURCE
 #LDFLAGS = -static -s
+
+CPPFLAGS = -D_POSIX_C_SOURCE=200809L -D_XOPEN_SOURCE=700 -D_BSD_SOURCE
 
 # OpenBSD 5.9+: use pledge(2)
 #CPPFLAGS += -DUSE_PLEDGE
