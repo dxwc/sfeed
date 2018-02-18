13269e18b4c8583e84dc0e78448b939d191b5120
Hiltjo Posthuma <hiltjo@codemadness.org>
2018-02-18 14:39:28 +0100

    sfeed_frames: code-style

--- sfeed_frames.c
+++ sfeed_frames.c
@@ -217,9 +217,9 @@ printfeed(FILE *fpitems, FILE *fpin, struct feed *f)
 		f->totalnew += isnew;
 		f->total++;
 
-                fprintf(fpitems, "%04d-%02d-%02d&nbsp;%02d:%02d ",
-                       tm->tm_year + 1900, tm->tm_mon + 1, tm->tm_mday,
-                       tm->tm_hour, tm->tm_min);
+		fprintf(fpitems, "%04d-%02d-%02d&nbsp;%02d:%02d ",
+		        tm->tm_year + 1900, tm->tm_mon + 1, tm->tm_mday,
+		        tm->tm_hour, tm->tm_min);
 
 		if (isnew)
 			fputs("<b><u>", fpitems);
