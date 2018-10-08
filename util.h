e6f4940e5f0fd9e2f53bdc4bac47a1f533f13185
Hiltjo Posthuma <hiltjo@codemadness.org>
2018-10-08 19:35:01 +0200

    util.h: remove unused macro ISUTF8()

--- util.h
+++ util.h
@@ -12,8 +12,6 @@ size_t strlcat(char *, const char *, size_t);
 #undef strlcpy
 size_t strlcpy(char *, const char *, size_t);
 
-#define ISUTF8(c) (((c) & 0xc0) != 0x80)
-
 /* feed info */
 struct feed {
 	char *        name;     /* feed name */
